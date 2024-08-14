1c31b0ef585993ebfc1f3497fdae4d3dc45304cd
zolynux <152818486+zolynux@users.noreply.github.com>
2024-08-14 00:10:09 -0500

    Actualizado APIs se cambia los animales de pokemon en dos botones está funcionando

--- react-03-apis-y-firebase/src/Components/Pokemon.jsx
+++ react-03-apis-y-firebase/src/Components/Pokemon.jsx
@@ -1,9 +1,46 @@
-const Pokemon = () => {
-  fetch("https://pokeapi.co/api/v2/pokemon/1").then((resp) => {
-    console.log(resp);
-  });
+import { useState, useEffect } from "react";
 
-  return <div>Pokemon</div>;
+const Pokemon = () => {
+  const [pokemon, setPokemon] = useState();
+  const [id, setId] = useState(1);
+
+  const handleAnterior = () => {
+    // if (id > 1) {
+    //   setId(id - 1);
+    // }
+    id > 1 && setId(id - 1);
+  };
+
+  const handleSiguiente = () => {
+    setId(id + 1);
+  };
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPokemon(data);
+      });
+  }, [id]);
+
+  return (
+    <>
+      {pokemon && (
+        <div>
+          <p>ID: {pokemon.id}</p>
+          <h2>{pokemon.name}</h2>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <br />
+          {id > 1 ? (
+            <button onClick={handleAnterior}>Anterior</button>
+          ) : (
+            <button disabled>Anterior</button>
+          )}
+          <button onClick={handleSiguiente}>Siguiente</button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Pokemon;
